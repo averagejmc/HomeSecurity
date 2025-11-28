0e57228c25d863314d063b26937ca9144688c388
John Michael Cunanan <averagejmc@cunanan.org>
2025-11-28 11:53:20 +0800

    added arduino code for real this time

---
+++ arduino-code/main/DoorSensor.cpp
@@ -0,0 +1,46 @@
+#include "DoorSensor.h"
+
+DoorSensor::DoorSensor(int pin)
+  : _pin(pin),
+    _lastStableState(HIGH),
+    _lastReading(HIGH),
+    _previousState(HIGH),
+    _lastDebounceTime(0) {}
+
+void DoorSensor::begin() {
+  pinMode(_pin, INPUT_PULLUP); 
+}
+
+void DoorSensor::update() {
+  int reading = digitalRead(_pin);
+
+  if (reading != _lastReading) {
+    _lastDebounceTime = millis(); 
+  }
+
+  if ((millis() - _lastDebounceTime) > _debounceDelay) {
+    _lastStableState = reading;
+  }
+
+  _lastReading = reading;
+}
+
+bool DoorSensor::hasStateChanged() {
+  if (_lastStableState != _previousState) {
+    _previousState = _lastStableState;
+    return true;
+  }
+  return false;
+}
+
+int DoorSensor::getState() const {
+  return _lastStableState;
+}
+
+String DoorSensor::getMessage() const {
+  if (_lastStableState == LOW) {
+    return "Door open";
+  } else {
+    return "Door closed!";
+  }
+}
---
+++ arduino-code/main/MotionSensor.h
@@ -0,0 +1,24 @@
+#ifndef MOTIONSENSOR_H
+#define MOTIONSENSOR_H
+
+#include <Arduino.h>
+
+class MotionSensor {
+public:
+  MotionSensor(int pin);
+  void begin();
+  void update();
+  bool hasStateChanged();
+  String getMessage() const;
+  int getState() const;
+
+private:
+  int _pin;
+  int _lastStableState;
+  int _lastReading;
+  int _previousState;
+  unsigned long _lastDebounceTime;
+  const unsigned long _debounceDelay = 200;
+};
+
+#endif
