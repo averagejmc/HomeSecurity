245549b455412891211a0f2b658119a680fa9ad9
Julian Gabriel L. Mercado <98792503+planetarioom@users.noreply.github.com>
2025-11-30 19:00:09 +0800

    separated logic for arduino and esp01

--- arduino-code/main/arduino/arduino.ino
+++ arduino-code/main/arduino/arduino.ino
@@ -1,23 +1,14 @@
 #include <SPI.h>
 #include <MFRC522.h>
-#include <SoftwareSerial.h>
-#include <avr/sleep.h>
-#include <avr/power.h>
 
 #include "DoorSensor.h"
 #include "MotionSensor.h"
 #include "RFIDSensor.h"
 
-const int ESP_RX_PIN = 3;
-const int ESP_TX_PIN = 4;
 const int RST_PIN = 22;   // Reset pin for RFID sensor
 const int SS_PIN = 5;   // SDA/SS pin for RFID sensotr
 const int motionPin = 14;  // PIR sensor
 const int doorPin   = 12;  // Magnetic door sensor
-const int switchPin = 2;
-const int ledPin = 9;
-
-SoftwareSerial espSerial(ESP_RX_PIN, ESP_TX_PIN);
 
 DoorSensor doorSensor(doorPin);
 MotionSensor motionSensor(motionPin);
@@ -29,22 +20,8 @@ unsigned long lastBlink = 0;
 const unsigned long blinkInterval = 500;
 bool ledState = LOW;
 
-enum SystemMode { AUTO = 0, MAINTENANCE, SLEEP, MODE_COUNT };
-volatile SystemMode currentMode = AUTO;
-
-void switchMode() {
-  currentMode = static_cast<SystemMode>((currentMode + 1) % MODE_COUNT);
-}
-
 void setup() {
   Serial.begin(115200);
-  espSerial.begin(9600);
-
-  pinMode(switchPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(switchPin), switchMode, FALLING);
-
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
 
   doorSensor.begin();
   motionSensor.begin();
@@ -54,55 +31,30 @@ void setup() {
 }
 
 void loop() {
+  if (!client.connected()) reconnectMQTT();
+  client.loop();
 
-  switch (currentMode) {
-    case AUTO:
-      motionSensor.update();
-      doorSensor.update();
-    
-      String uid = rfidSensor.read();
-    
-      if (millis() - lastLooptTime >= loopInterval) {
-        lastLooptTime = millis();
-    
-        String motionState = motionSensor.getMessage();
-        String doorState = doorSensor.getMessage();
+  if (millis() - lastLooptTime => loopInterval) {
+    lastLooptTime = millis();
 
-        // print data in serial in format 'mode,motion,door,rfid'
-        String dataLine = String(currentMode) + "," +
-                                      motionState + "," +
-                                      doorState + "," +
-                                      uid;
-    
-        espSerial.println(dataLine);
-      }
-      break;
+    motionSensor.update();
+    if (motionSensor.hasStateChanged()) {
+      String motionMsg = motionSensor.getMessage();
+      Serial.println(motionMsg);
+      client.publish("home/motion", motionMsg.c_str());
+    }
 
-    case MAINTENANCE:
-      if (millis() - lastBlink >= blinkInterval) {
-        lastBlink = millis();
-        ledState = !ledState;
-        digitalWrite(ledPin, ledState);
-      }
+    doorSensor.update();
+    if (doorSensor.hasStateChanged()) {
+        String doorMsg = doorSensor.getMessage();
+        Serial.println(doorMsg);
+        client.publish("home/door", doorMsg.c_str());
+    }
 
-      motionSensor.update();
-      doorSensor.update();
-      rfidSensor.read();
-      break;
-
-    case SLEEP:
-      digitalWrite(ledPin, HIGH); 
-
-      set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-      sleep_enable();
-      
-      sleep_cpu();
-
-      sleep_disable(); 
-      
-      break;
-
-    default:
-      break;
+    String uid = rfidSensor.read();
+    if (!uid.isEmpty()) {
+        Serial.println("RFID UID: " + uid);
+        client.publish("home/rfid", uid.c_str());
+    }
   }
 }
