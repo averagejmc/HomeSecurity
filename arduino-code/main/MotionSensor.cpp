c57ab45095d88da65cd561fa6a2d01d02a7b8945
Julian Gabriel L. Mercado <98792503+planetarioom@users.noreply.github.com>
2025-11-30 15:04:06 +0800

    fixed commit conflicts

--- arduino-code/main/MotionSensor.cpp
+++ arduino-code/main/MotionSensor.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "MotionSensor.h"
 
 MotionSensor::MotionSensor(int pin)
@@ -45,51 +44,3 @@ String MotionSensor::getMessage() const {
     return "No motion";
   }
 }
-=======
-#include "MotionSensor.h"
-
-MotionSensor::MotionSensor(int pin)
-  : _pin(pin),
-    _lastStableState(LOW),
-    _lastReading(LOW),
-    _previousState(LOW),
-    _lastDebounceTime(0) {}
-
-void MotionSensor::begin() {
-  pinMode(_pin, INPUT);
-}
-
-void MotionSensor::update() {
-  int reading = digitalRead(_pin);
-
-  if (reading != _lastReading) {
-    _lastDebounceTime = millis();
-  }
-
-  if ((millis() - _lastDebounceTime) > _debounceDelay) {
-    _lastStableState = reading;
-  }
-
-  _lastReading = reading;
-}
-
-bool MotionSensor::hasStateChanged() {
-  if (_lastStableState != _previousState) {
-    _previousState = _lastStableState;
-    return true;
-  }
-  return false;
-}
-
-int MotionSensor::getState() const {
-  return _lastStableState;
-}
-
-String MotionSensor::getMessage() const {
-  if (_lastStableState == HIGH) {
-    return "Motion detected!";
-  } else {
-    return "No motion";
-  }
-}
->>>>>>> 27dc04e0428cb78814e1be60a14c747cebfc24e0
--- arduino-code/main/RFIDSensor.h
+++ arduino-code/main/RFIDSensor.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef RFIDSENSOR_H
 #define RFIDSENSOR_H
 
@@ -18,24 +17,3 @@ private:
 };
 
 #endif
-=======
-#ifndef RFIDSENSOR_H
-#define RFIDSENSOR_H
-
-#include <Arduino.h>
-#include <SPI.h>
-#include <MFRC522.h>
-
-class RFIDSensor {
-public:
-  RFIDSensor(byte ssPin, byte rstPin);
-  void begin();
-  String read();
-
-private:
-  MFRC522 _mfrc522;
-  MFRC522::MIFARE_Key _key;
-};
-
-#endif
->>>>>>> 27dc04e0428cb78814e1be60a14c747cebfc24e0
